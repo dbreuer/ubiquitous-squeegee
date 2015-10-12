ef0981b97e1cf4e0696b1ecaf536b1c8a06c382f
David Breuer <dbreuer83@gmail.com>
2015-10-12 17:21:57 +0100

    init

---
+++ app/components/aat-skrollr/src/aat-scrollr.js
@@ -0,0 +1,36 @@
+//aat-scrollr.js
+/**
+ * Created by David Breuer on 12/10/15.
+ *
+ * @file aat-scrollr.js
+ * @description
+ *
+ */
+
+function AATSkrollrController() {
+    console.log("Angular you fckr");
+}
+
+
+angular
+    .module('aatScrollr', ['ngRoute', 'sn.skrollr'])
+    .config(function(snSkrollrProvider, $routeProvider, $locationProvider) {
+        snSkrollrProvider.config = { smoothScrolling: true};
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.view.html',
+                controller: 'AATSkrollrController',
+                controllerAs: 'AATSC'
+            })
+            .otherwise('/');
+
+        // configure html5 to get links working on jsfiddle
+        $locationProvider.html5Mode(true);
+    })
+
+// 2. initialise skrollr at runtime
+    .run(["snSkrollr", function(snSkrollr) {
+        snSkrollr.init();
+    }])
+    .controller('AATSkrollrController', AATSkrollrController);
